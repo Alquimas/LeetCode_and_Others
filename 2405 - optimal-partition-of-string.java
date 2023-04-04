2870528c747e00d2ff3826a29617514f1ac1613b
Alquimas <119738123+Alquimas@users.noreply.github.com>
2023-04-04 18:53:09 -0300

    Update 2405 - optimal-partition-of-string.java

--- 2405 - optimal-partition-of-string.java
+++ 2405 - optimal-partition-of-string.java
@@ -2,24 +2,19 @@ class Solution
 {
     public int partitionString(String s) 
     {
-        char[] max = new char[s.length()];
         char[] string = s.toCharArray();
         int k=1;
         int l=0;
         for(int i = 0; i<s.length(); i++)
         {
-            if(i > 0)
+            for(int j = l; j<i; j++)
             {
-                for(int j = l; j<i; j++)
+                if(string[j] == string[i])
                 {
-                    if(max[j] == string[i])
-                    {
-                        k++;
-                        l=i;
-                    }
+                    k++;
+                    l=i;
                 }
             }
-            max[i] = string[i];
         }
         return k;
     }
