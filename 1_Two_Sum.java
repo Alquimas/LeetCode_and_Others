085d1a27f4b65ea9a5685e2bb03f5c6856045c92
Alquimas <119738123+Alquimas@users.noreply.github.com>
2023-04-16 11:41:01 -0300

    Adicionado soluçaõ otimizada

--- 1_Two_Sum.java
+++ 1_Two_Sum.java
@@ -8,12 +8,31 @@ class Solution {
             {
                 if(nums[i] + nums[j] == target)
                 {
-                    retorno[0] = j;
-                    retorno[1] = i;
-                    return retorno;
+                    return new int[] {i, j};
                 }
             }
         }
-        return retorno;
+        //sempre ha solucao: o java que necessita de alguma opcao de retorno fora dos desvios
+        return null;
     }  
 }
+
+//solucao com hash table
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) 
+    {
+        //lembre que um Map<K, L> é composto por K = keys e L = elementos
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) 
+        {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) 
+            {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        //sempre ha solucao: o java que necessita de alguma opcao de retorno fora dos desvios
+        return null;
+    }
