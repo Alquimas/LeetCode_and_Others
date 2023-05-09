5d1341b24babcce513826fa1551f8881ed19eb4c
Alquimas <119738123+Alquimas@users.noreply.github.com>
2023-05-09 09:44:25 -0300

    Create 1137_N-th_Tribonacci_Number.java

---
+++ 1137_N-th_Tribonacci_Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 1;
+        int n4 = 0;
+    
+       for(int i = 2; i < n; i++){
+          n4 = n1 + n2 + n3;
+          n1 = n2;
+          n2 = n3;
+          n3 = n4;
+       }
+       return n4;
+    }
+}
