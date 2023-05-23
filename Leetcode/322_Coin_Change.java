acac2fc9dccf8575587a62b08a1c41e76a1e581e
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-05-23 18:29:14 -0300

    Create 322_Coin_Change.java

---
+++ Leetcode/322_Coin_Change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int infty = amount+1;
+        int[][] dp = new int[coins.length+1][amount+1];
+        for(int i = 1; i < amount + 1; i++)
+        {
+            dp[0][i] = infty;
+        }
+        for(int i = 0; i < coins.length + 1; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i < coins.length + 1; i++)
+        {
+            for(int j = 1; j < amount + 1; j++)
+            {
+                if(j < coins[i-1])
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i-1]] + 1);
+                }
+            }
+        }
+
+        int x = (dp[coins.length][amount] > amount) ? -1 : dp[coins.length][amount];
+
+        return x;
+
+    }
+}
