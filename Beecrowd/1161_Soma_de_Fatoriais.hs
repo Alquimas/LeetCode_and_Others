8daec4f0b5d4e50002faab09e4b310615b18534f
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-07-31 13:44:15 -0300

    Create 1161_Soma_de_Fatoriais.hs

---
+++ Beecrowd/1161_Soma_de_Fatoriais.hs
@@ -0,0 +1,12 @@
+fatsum :: Integer -> Integer -> Integer
+fatsum a b = fat a + fat b
+
+fat :: Integer -> Integer
+fat 0 = 1
+fat n = n * fat (n - 1)
+
+prints :: [Integer] -> String
+prints [] = []
+prints (x:y:xs) = show (fatsum x y) ++ "\n" ++ prints xs
+
+main = interact $ prints . map read. words
