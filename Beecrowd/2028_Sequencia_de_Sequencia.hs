c8b564759436f2c275c806dc6830d4f12e305ca0
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-08-02 19:12:04 -0300

    Update 2028_Sequencia_de_Sequencia.hs

--- Beecrowd/2028_Sequencia_de_Sequencia.hs
+++ Beecrowd/2028_Sequencia_de_Sequencia.hs
@@ -9,8 +9,8 @@ showList' (x:xs) = (show x) ++ " " ++ showList' xs
 
 function :: Int -> [Int] -> String
 function _ [] = []
-function a (0:xs) = "Caso " ++ show a ++ ": 1 numero\n0\n" ++ function (a + 1) xs
-function a (x:xs) = "Caso " ++ show a ++ ": " ++ show c ++ " numeros\n" ++ showList' b ++ "\n" ++ function (a + 1) xs
+function a (0:xs) = "Caso " ++ show a ++ ": 1 numero\n0\n\n" ++ function (a + 1) xs
+function a (x:xs) = "Caso " ++ show a ++ ": " ++ show c ++ " numeros\n" ++ showList' b ++ "\n\n" ++ function (a + 1) xs
     where 
         b = sequence' x
         c = length b
