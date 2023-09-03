3bd3f31292b8aba9c56665c0a268abfd99d8fc9f
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-09-02 21:44:04 -0300

    Update 3160_Amigos.hs

--- Beecrowd/3160_Amigos.hs
+++ Beecrowd/3160_Amigos.hs
@@ -3,10 +3,16 @@ import Data.List
 function :: [String] -> String
 function (a:b:c:_)
     | c == "nao" = a ++ " " ++ b 
-    | otherwise = con (words a) b c
+    | otherwise = con (words a) (words b) c
     
-con :: [String] -> String -> String -> String
-con a b c = (unwords (takeWhile (/= c) (a))) ++ " " ++ b ++ " " ++ (unwords (dropWhile (/= c) a))
+con :: [String] -> [String] -> String -> String
+con a b c
+    | x == [] = (unwords b) ++ " " ++ y
+    | y == [] = x ++ " " ++ (unwords b) 
+    | otherwise = x ++ " " ++ (unwords b) ++ " " ++ y
+    where 
+        x = (unwords (takeWhile (/= c) a)) 
+        y = (unwords (dropWhile (/= c) a))
     
 main = interact
        $ (++"\n")
