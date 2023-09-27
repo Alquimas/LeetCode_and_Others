c5270c167f6602401c8e1142929d450b0c070de9
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-09-26 21:32:24 -0300

    Update 1878_A.java

--- Codeforces/1878_A.java
+++ Codeforces/1878_A.java
@@ -1,31 +1,31 @@
-    import java.util.*;
-     
-    public class Main
-    {
-        public static void main (String[] args) {
-            Scanner sc = new Scanner(System.in);
+import java.util.*;
+
+public class Main
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int k = Integer.valueOf(sc.nextLine());
+        
+        outerLoop:
+        for(int i = 0; i < k; i++)
+        {
+            int a = Integer.valueOf(sc.next());
+            int b = Integer.valueOf(sc.next());
+            sc.nextLine();
             
-            int k = Integer.valueOf(sc.nextLine());
-            
-            outerLoop:
-            for(int i = 0; i < k; i++)
+            for(int j = 0; j < a; j++)
             {
-                int a = Integer.valueOf(sc.next());
-                int b = Integer.valueOf(sc.next());
-                sc.nextLine();
-                
-                for(int j = 0; j < a; j++)
+                int m = Integer.valueOf(sc.next());
+                if(b == m)
                 {
-                    int m = Integer.valueOf(sc.next());
-                    if(b == m)
-                    {
-                        System.out.println("YES");
-                        sc.nextLine();
-                        continue outerLoop;
-                    }
+                    System.out.println("YES");
+                    sc.nextLine();
+                    continue outerLoop;
                 }
-                System.out.println("NO");
-                sc.nextLine();
             }
+            System.out.println("NO");
+            sc.nextLine();
         }
     }
+}
