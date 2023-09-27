ad53fd3a7559178a52f98ed7f4d6f7ca61bafd07
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-09-26 21:41:12 -0300

    Update and rename contest_1873_B.hs to 1873_B.hs

---
+++ Codeforces/1873_B.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+parsing :: Int -> [Int] -> String
+parsing 0 _ = []
+parsing x (y:ys) = solve (take y ys) ++ "\n"
+                   ++ parsing (x-1) (drop y ys)
+                   
+solve :: [Int] -> String
+solve = show . (foldr (*) 1) . add . sort
+    where 
+        add xs = ((head xs) + 1) : (tail xs)
+
+main = interact
+       $ (\(x:xs) -> parsing x xs)
+       . map read
+       . words
