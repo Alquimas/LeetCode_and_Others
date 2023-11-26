b2f9b724e54cbbf21505841662077cc4f7eab9bc
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-11-26 18:31:51 -0300

    Create 1001_Extremamente_Basico.cpp

---
+++ Beecrowd/1001_Extremamente_Basico.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a, b;
+    
+    cin >> a;
+    
+    cin >> b;
+    
+    cout << "X = " << a + b << "\n";
+    
+    return 0;
+}
