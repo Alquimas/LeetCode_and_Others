d687d3a8fafd00d8ea29c2da3c347a83d5a1616f
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-11-26 18:46:19 -0300

    Create 1002_Area_do_Circulo.cpp

---
+++ Beecrowd/1002_Area_do_Circulo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double a;
+    const double pi = 3.14159;
+
+    cin >> a;
+
+    cout.precision(4);
+
+    cout << "A=" << fixed << a*a*pi << "\n";
+
+    return 0;
+}
